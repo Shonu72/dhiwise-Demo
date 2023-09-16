f03d051eb63dcfae4d8c98bc49c277d21b7a8a1d
Shonu72 <sonu.shour77@gmail.com>
2023-09-16 16:40:28 +0000

    Code Synced by DhiWise

---
+++ lib/data/apiClient/api_client.dart
@@ -0,0 +1,3 @@
+import 'package:techlax_blog/core/app_export.dart';
+
+class ApiClient extends GetConnect {}
---
+++ lib/presentation/drawer_menu_draweritem/controller/drawer_menu_controller.dart
@@ -0,0 +1,16 @@
+import 'package:techlax_blog/core/app_export.dart';
+import 'package:techlax_blog/presentation/drawer_menu_draweritem/models/drawer_menu_model.dart';
+
+class DrawerMenuController extends GetxController {
+  Rx<DrawerMenuModel> drawerMenuModelObj = DrawerMenuModel().obs;
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+}
